1190498a0e1ee487ee1f277b562a8a409e89da6a
Tanuja Gurung <67540467+TanujaGurung@users.noreply.github.com>
2021-11-24 18:53:27 +0530

    committed

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,12 +14,13 @@ export default class Home extends React.Component {
   componentDidMount() {
     this.fetchApi();
   }
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.selectedId !== this.state.selectedId) {
-  //     this.changeSelectedAnime(id);
-  //   }
-  // }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.search !== this.state.search) {
+      this.fetchApi();
+    }
+  }
   // 3f80fbc0-be7e-4833-b796-e3c6e62371c1
+
   changeSelectedAnime = (id) => {
     this.setState({ selectedId: id });
     // console.log(this.state.selectedId);
@@ -31,17 +32,27 @@ export default class Home extends React.Component {
   };
 
   fetchApi = async () => {
-    await axios
-      .get("https://api.aniapi.com/v1/random/anime/100")
-      .then((res) => {
-        this.setState({ data: res.data.data.slice(0, 30) });
-        console.log(this.state.data);
-      });
+    if (this.state.search === "") {
+      await axios
+        .get("https://api.aniapi.com/v1/random/anime/40")
+        .then((res) => {
+          this.setState({ data: res.data.data });
+          console.log(this.state.data);
+        });
+    } else {
+      await axios
+        .get("https://api.aniapi.com/v1/anime?title=" + this.state.search)
+        .then((res) => {
+          this.setState({ data: res.data.data.documents });
+          console.log(this.state.data);
+        });
+    }
   };
 
   render() {
     const { data, search } = this.state;
-    console.log(search);
+    //console.log(search);
+    console.log(document.location.hash);
 
     return (
       <BrowserRouter>
@@ -54,37 +65,22 @@ export default class Home extends React.Component {
                 <div class="container-fluid">
                   <div class="row">
                     {data.length ? (
-                      data
-                        .filter((val) => {
-                          if (search == "") {
-                            return val;
-                          } else if (
-                            val.titles.en
-                              .toLowerCase()
-                              .includes(search.toLowerCase()) ||
-                            val.descriptions.en
-                              .toLowerCase()
-                              .includes(search.toLowerCase())
-                          ) {
-                            return val;
-                          }
-                        })
-                        .map((list) => (
-                          <Card
-                            key={list.anilist_id}
-                            genres={list.genres[0]}
-                            image={list.cover_image}
-                            title={list.titles}
-                            episodesCount={list.episodes_count}
-                            description={list.description}
-                            trailer_url={list.trailer_url}
-                            season_year={list.season_year}
-                            banner_image={list.banner_image}
-                            cover_color={list.cover_color}
-                            id={list.id}
-                            changeSelectedAnime={this.changeSelectedAnime}
-                          />
-                        ))
+                      data.map((list) => (
+                        <Card
+                          key={list.anilist_id}
+                          genres={list.genres[0]}
+                          image={list.cover_image}
+                          title={list.titles}
+                          episodesCount={list.episodes_count}
+                          description={list.description}
+                          trailer_url={list.trailer_url}
+                          season_year={list.season_year}
+                          banner_image={list.banner_image}
+                          cover_color={list.cover_color}
+                          id={list.id}
+                          changeSelectedAnime={this.changeSelectedAnime}
+                        />
+                      ))
                     ) : (
                       <div>Loading... </div>
                     )}
