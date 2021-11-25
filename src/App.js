1de8fc42ed3488b2be80dabf9b1a46064b50ad0d
Tanuja Gurung <67540467+TanujaGurung@users.noreply.github.com>
2021-11-25 10:48:15 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Home from "./components/Home";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "anime world - find any anime here";
+  }, []);
   return (
     <div className="App">
       <Home />
